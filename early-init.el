3a49a107999745b46038ec3799a7ebc31305f4cb
Loris Lucido <loris.lucido@atos.net>
2022-06-02 10:34:26 +0200

    dotfiles init

---
+++ early-init.el
@@ -0,0 +1,35 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; File introduced in emacs 27.1, it is run before init.el, before package and
+;; UI initialization.
+
+
+(defconst me/cache-directory
+  (concat user-emacs-directory ".cache/")
+  "Directory where all cache files should be saved.")
+
+;; Compile .eln files in cache folder
+(setq native-comp-eln-load-path
+      (append (list (concat me/cache-directory "eln"))
+              (cdr native-comp-eln-load-path)))
+(setq-default package-native-compile t
+              native-comp-async-report-warnings-errors 'silent)
+
+;; from doomacs
+(unless (or (daemonp) noninteractive)
+  ;; Emacs really shouldn't be displaying anything until it has fully started
+  ;; up. This saves a bit of time.
+  (setq-default inhibit-redisplay t
+                inhibit-message t)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (setq-default inhibit-redisplay nil
+                            inhibit-message nil)
+              (redisplay))))
+
+;; Contrary to what many Emacs users have in their configs, you don't need
+;; more than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+
+;; set-language-enviornment sets default-input-method, which is unwanted
+(setq default-input-method nil)
---
+++ init.el
@@ -0,0 +1,51 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Default init and cache location
+(defconst me-config-file
+  (concat user-emacs-directory "dotmacs.el")
+  "The location of the generated elisp config file.")
+
+(defconst me-config-file-org
+  (concat user-emacs-directory "dotmacs.org")
+  "The location of the `org-mode' config file.")
+
+;; Already defined with `early-init.el'
+(when (version< emacs-version "27.1")
+  (defconst me/cache-directory
+    (concat user-emacs-directory ".cache/")
+    "Directory where all cache files should be saved."))
+
+(defun me/cache-concat (name)
+  "Return the absolute path of NAME under `me/cache-directory'."
+  (concat me/cache-directory name))
+
+(defun me/reload-user-org-file ()
+  "Reload emacs configuration."
+  (interactive)
+  (org-babel-load-file me-config-file-org))
+
+(let ((default-directory user-emacs-directory)
+      (file-name-handler-alist nil)
+      (gc-cons-percentage .6)
+      (gc-cons-threshold most-positive-fixnum)
+      (read-process-output-max (* 1024 1024)))
+
+  ;; Disable that pesky echo message
+  (setq inhibit-startup-echo-area-message user-login-name)
+
+  ;; Mark safe variables early so that tangling won't break
+  (put 'after-save-hook 'safe-local-variable
+       (lambda (value) (equal value '(org-babel-tangle t))))
+  (put 'display-line-numbers-width 'safe-local-variable 'integerp)
+
+  ;; Tangle and compile if necessary only, then load the configuration
+  (me/reload-user-org-file)
+
+  ;; Set the working directory to home
+  (cd "~/")
+
+  ;; Collect garbage when all else is done
+  (garbage-collect))
+
+;; Setup debug backtrace
+                                        ;(toggle-debug-on-error 1)
